2e8d20f30b3c524a62512b078b34ff7b56ac7f58
Wellington Kato <wellingtonkato@gmail.com>
2018-09-16 22:39:38 -0300

    banco de dados h2 e criação automatica de base de dados

--- src/main/java/com/kato/cursomc/domain/Categoria.java
+++ src/main/java/com/kato/cursomc/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.kato.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	//da certo com maioria dos bancos de dados
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
