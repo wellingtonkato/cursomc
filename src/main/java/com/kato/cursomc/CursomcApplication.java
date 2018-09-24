309dd5a0f60e28acba84da5fd2685ea6afe64a97
Wellington Kato <wellingtonkato@gmail.com>
2018-09-23 21:05:01 -0300

    Cliente, TipoCliente, telefones, endereços

--- src/main/java/com/kato/cursomc/CursomcApplication.java
+++ src/main/java/com/kato/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kato.cursomc.domain.Categoria;
 import com.kato.cursomc.domain.Cidade;
+import com.kato.cursomc.domain.Cliente;
+import com.kato.cursomc.domain.Endereco;
 import com.kato.cursomc.domain.Estado;
 import com.kato.cursomc.domain.Produto;
+import com.kato.cursomc.domain.enums.TipoCliente;
 import com.kato.cursomc.repositories.CategoriaRepository;
 import com.kato.cursomc.repositories.CidadeRepository;
+import com.kato.cursomc.repositories.ClienteRepository;
+import com.kato.cursomc.repositories.EnderecoRepository;
 import com.kato.cursomc.repositories.EstadoRepository;
 import com.kato.cursomc.repositories.ProdutoRepository;
 
@@ -28,6 +33,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -64,6 +75,18 @@ public class CursomcApplication implements CommandLineRunner {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "5464646464", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("985468464", "754187487"));
+		
+		Endereco e1 = new Endereco(null, "Rua FLores", "300", "Apto 303", "Jardim", "38220824", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av Matos", "105", "sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 		
 		
--- src/main/java/com/kato/cursomc/domain/Endereco.java
+++ src/main/java/com/kato/cursomc/domain/Endereco.java
@@ -2,9 +2,19 @@ package com.kato.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Endereco implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String logradouro;
 	private String numero;
@@ -12,15 +22,20 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
+	@ManyToOne
+	@JoinColumn(name = "cidade_id")
 	private Cidade cidade;
 	
 	public Endereco() {		
 	}
 
 	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
-			Cliente cliente) {
+			Cliente cliente, Cidade c1) {
 		super();
 		this.id = id;
 		this.logradouro = logradouro;
